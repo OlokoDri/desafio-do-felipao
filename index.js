9234ef68420b95dc830ff6b0dce23dfff8776266
OlokoDri <adriano@gmail.com>
2026-01-07 19:17:01 -0300

    atualizando projeto

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-// Nome
-let NomeDoHeroi = "Kirito";
+// Herói 1 - Kirito
+let nomeHeroi1 = "Kirito";
+let xpHeroi1 = 6000;
+let nivelHeroi1 = "";
 
-// XP
-let XpDoHeroi = 8999; 
-// Verificação do nível
-let NivelDoHeroi = "";
-
-if (XpDoHeroi >= 8001 && XpDoHeroi <= 9000) {
-    NivelDoHeroi = "Ascendente";
+if (xpHeroi1 >= 8001 && xpHeroi1 <= 9000) {
+    nivelHeroi1 = "Ascendente";
+} else if (xpHeroi1 >= 5001 && xpHeroi1 <= 7000) {
+    nivelHeroi1 = "Ouro";
+} else if (xpHeroi1 >= 2001 && xpHeroi1 <= 5000) {
+    nivelHeroi1 = "Prata";
 } else {
-    NivelDoHeroi = "Outro nível";
+    nivelHeroi1 = "Outro nível";
 }
 
-// Exibir informações do herói
-console.log("O nome do herói é: " + NomeDoHeroi);
-console.log("O XP do herói é: " + XpDoHeroi);
-console.log("O nível do herói é: " + NivelDoHeroi);
+console.log("Herói: " + nomeHeroi1);
+console.log("XP: " + xpHeroi1);
+console.log("Nível: " + nivelHeroi1);
+console.log("");
 
+
+// Herói 2 - Ainz
+let nomeHeroi2 = "Ainz";
+let xpHeroi2 = 8500;
+let nivelHeroi2 = "";
+
+if (xpHeroi2 >= 8001 && xpHeroi2 <= 9000) {
+    nivelHeroi2 = "Ascendente";
+} else if (xpHeroi2 >= 5001 && xpHeroi2 <= 7000) {
+    nivelHeroi2 = "Ouro";
+} else if (xpHeroi2 >= 2001 && xpHeroi2 <= 5000) {
+    nivelHeroi2 = "Prata";
+} else {
+    nivelHeroi2 = "Outro nível";
+}
+
+console.log("Herói: " + nomeHeroi2);
+console.log("XP: " + xpHeroi2);
+console.log("Nível: " + nivelHeroi2);
+console.log("");
+
+
+// Herói 3 - Dr. Stone
+let nomeHeroi3 = "Dr. Stone";
+let xpHeroi3 = 3500;
+let nivelHeroi3 = "";
+
+if (xpHeroi3 >= 8001 && xpHeroi3 <= 9000) {
+    nivelHeroi3 = "Ascendente";
+} else if (xpHeroi3 >= 5001 && xpHeroi3 <= 7000) {
+    nivelHeroi3 = "Ouro";
+} else if (xpHeroi3 >= 2001 && xpHeroi3 <= 5000) {
+    nivelHeroi3 = "Prata";
+} else {
+    nivelHeroi3 = "Outro nível";
+}
+
+console.log("Herói: " + nomeHeroi3);
+console.log("XP: " + xpHeroi3);
+console.log("Nível: " + nivelHeroi3);
+console.log("");
